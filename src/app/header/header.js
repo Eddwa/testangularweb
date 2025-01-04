eec2c58503f22ef0a6fb2f054284eb5acfee0361
wilson <wilson384138@gmail.com>
2025-01-05 01:24:21 +0800

    retry push

---
+++ src/app/header/header.js
@@ -0,0 +1,9 @@
+console.log("header.js is working");
+let intro = document.querySelector(".intro");
+let introLogo = document.querySelector(".intro-logo");
+
+window.addEventListener("DOMContentLoaded",()=>{
+    setTimeout(()=>{
+        intro.style.top = '-100vh';
+    },2000)
+})
